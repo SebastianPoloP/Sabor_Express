03276bbb5cd444bbbcad17443e67c04ff38368ca
SebastianPoloP <jspolo2@misena.edu.com>
2024-11-12 16:13:12 -0500

    Comentarios adicionales

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { LoginController } from "../controller/login.js";
-
+// Ruta para el login
 export const createLoginRouter = ({ loginModel }) => {
   const loginRouter = Router();
+  // Uso del controlador y el modelo que va a usar
   const loginController = new LoginController({ loginModel });
-
+  // Metodo post para logear a una persona
   loginRouter.post('/', loginController.loginUser)
   return loginRouter;
 }
